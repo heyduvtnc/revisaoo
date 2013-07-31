87371069fcd4e42db086fc8db1c405d8dbeae1a9
larissa_mezari <larissa_mezari@10.1.48.227>
2013-07-31 15:54:59 -0300

    Criando a classe Pessoa jurídica

---
+++ Revisaoo/src/br/senai/sc/model/negocio/PessoaJuridica.java
@@ -0,0 +1,42 @@
+
+package br.senai.sc.model.negocio;
+
+/**
+ * Classe que represeta as pessoas jurídicas do sistema
+ * @version 1.0 31/07/13
+ * @author larissa_mezari
+ */
+public class PessoaJuridica {
+    private String cnpj;
+    private String razaoSocial;
+    private String nomeFantasia;
+
+    public PessoaJuridica() {
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+    
+    
+}
