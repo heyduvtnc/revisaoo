6b8fbb4048c779b8119d6e5031f2ab06cef071ea
larissa_mezari <larissa_mezari@10.1.48.227>
2013-07-31 15:38:32 -0300

    Criando a classe funcionario

---
+++ Revisaoo/src/br/senai/sc/model/negocio/Funcionario.java
@@ -0,0 +1,50 @@
+package br.senai.sc.model.negocio;
+
+/**
+ * Classe que representa os funcionários do software
+ * @version 1.0 31/07/13
+ * @author larissa_mezari
+ */
+public class Funcionario {
+    private double salario;
+    private String dataAdmissao;
+    private String ctps;
+    private String cargo;
+
+    public Funcionario() {
+        
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getDataAdmissao() {
+        return dataAdmissao;
+    }
+
+    public void setDataAdmissao(String dataAdmissao) {
+        this.dataAdmissao = dataAdmissao;
+    }
+
+    public String getCtps() {
+        return ctps;
+    }
+
+    public void setCtps(String ctps) {
+        this.ctps = ctps;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+        
+}
